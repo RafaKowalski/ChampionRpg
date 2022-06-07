471e0be4cc0d7df811e3d14bfcb88cf0793938aa
Rafael Kowalski <rafaelmk06@gmail.com>
2022-06-06 22:45:01 -0300

    Add project files.

---
+++ ChampionRpg/Entities/Champion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChampionRpg.Entities
+{
+    public class Champion
+    {
+        public string Name { get; set; }
+        public int Hp { get; set; }
+        public Champion()
+        {
+        }
+        public Champion(string name, int hp)
+        {
+            Name = name;
+            Hp = hp;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + "\nHp: " + Hp;
+        }
+    }
+}
---
+++ ChampionRpg/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using ChampionRpg.Entities;
+
+namespace ChampionProgram;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Champion championOne, championTwo;
+
+        Console.WriteLine("Digite o nome do primeiro campeao: ");
+        string nameOne = Console.ReadLine();
+
+        Console.WriteLine("Digite a quantidade de pontos vitais do primeiro campeao: ");
+        int hpOne = int.Parse(Console.ReadLine());
+
+        championOne = new Champion(nameOne, hpOne);
+
+        Console.WriteLine("Digite o nome do segundo campeao: ");
+        string nameTwo = Console.ReadLine();
+
+        Console.WriteLine("Digite a quantidade de pontos vitais do segundo campeao: ");
+        int hpTwo = int.Parse(Console.ReadLine());
+
+        championTwo = new Champion(nameTwo, hpTwo);
+
+        Console.WriteLine(championOne);
+        Console.WriteLine(championTwo);
+    }
+}
