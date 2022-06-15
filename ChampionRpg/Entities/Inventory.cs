f0f975775071820273a44189f6b6e05424111ef5
Rafael Kowalski <rafaelmk06@gmail.com>
2022-06-14 23:11:34 -0300

    Criando classe Inventory

---
+++ ChampionRpg/Entities/Inventory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChampionRpg.Entities
+{
+    public class Inventory
+    {
+        public List<Item> Items;
+
+        public Inventory()
+        {
+            Items = new List<Item>();
+        }
+
+        public List<Item> AdicionandoItem(Item item)
+        {
+            List<Item> list = new List<Item>();
+            list.Add(item);
+            return list;
+        }
+
+        public List<Item> PerdendoItem(Item item)
+        {
+            List <Item> list = new List<Item>();
+            list.Remove(item);
+            return list;
+        }
+    }
+}
